194c2b94a3feee274eefa4ca5cf4a23b8c12c567
adam-sch <adam.schlabach@gmail.com>
2024-02-14 12:09:39 -0800

    Project Start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,214 @@
-import logo from './logo.svg';
 import './App.css';
+import { skills } from './skills.js';
+import iconPhone from './images/square-phone-solid.svg'
+import iconMail from './images/square-envelope-solid.svg'
+import iconLinkedIn from './images/linkedin.svg'
+import iconMap from './images/map-location-dot-solid.svg'
+import iconGitHub from './images/square-github.svg'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="main bg-gray-300 pb-10">
+
+      <header className="header pt-10 text-gray-700">
+        <div className="container mx-auto">
+          <h1 className="header__heading text-5xl font-bold text-center font-serif mt-3">
+            Adam Schlabach
+          </h1>
+
+          <div className="header__title-group flex justify-center py-7 mb-2">
+            <ProfileTitle
+              title="VP of Engineering"
+            />
+            <ProfileTitle
+              title="Corporate Website Manager"
+            />
+            <ProfileTitle
+              title="Front-end Developer"
+            />
+            <ProfileTitle
+              title="All Things B2B Website"
+            />
+          </div>
+        </div>
       </header>
+
+      <div className="profile-info">
+        <div className="container mx-auto">
+          <div className="profile-info__bar bg-gray-700 py-5 px-10 rounded-md size-full flex justify-center">
+            <ProfileInfo
+              icon={iconPhone}
+              content="503.970.0038"
+            />
+            <ProfileInfo
+              icon={iconMail}
+              content="adam.schlabach@gmail.com"
+              url="mailto:adam.schlabach@gmail.com"
+            />
+            <ProfileInfo
+              icon={iconLinkedIn}
+              content="adamschlabach"
+              url="https://www.linkedin.com/in/adamschlabach/"
+            />
+            <ProfileInfo
+              icon={iconGitHub}
+              content="adam-sch"
+              url="https://github.com/adam-sch"
+            />
+            <ProfileInfo
+              icon={iconMap}
+              content="Portland, OR"
+            />
+          </div>
+        </div>
+      </div>
+
+      <Section classes="skills">
+        <SectionHeading>Skills and Technologies:</SectionHeading>
+        <Skills />
+        <SkillsSecondary />
+      </Section>
+
+      <Section classes="what-i-can-do">
+        <SectionHeading>What I can do:</SectionHeading>
+        <Skills />
+      </Section>
+
+      <Section classes="about">
+        <SectionHeading>A little about me:</SectionHeading>
+        <p>I have a strong background in website development with a core focus on marketing-driven corporate websites. In my current role I work almost exclusively alongside marketing teams from just-funded and maybe a bit overwhelmed startups to CMOs of mature publicly traded companies to build, manage and maintain their web presence.</p>
+
+        <p>My expertise spans both creative and technical aspects, enabling me to quickly conceptualize and execute solutions. I am adaptable and keen on learning to expand my skill sets and stay updated with evolving web technologies, allowing me to quickly research and adopt new tools, methods, frameworks and tech stacks.</p>
+
+        <p>IM LOOKING FOR... Either a website to build and own with a marketing team or to work alongside a team of developers. My current role involves quickly relaunching sites and sending them off to clients, I feel my expertise could shine on fewer projects and utilize the culmination of my experience and implemented features. Marketing teams are where I’ve had the most experience but I can comfortably work alongside back-end and other front-end developers.</p>
+      </Section>
+
+      <Section classes="portfolio bg-stone-950 text-white py-16 text-center mt-10 text-lg">
+        <h2 className="text-3xl uppercase font-bold mb-8">A note on my Portfolio:</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </Section>
+
+      <Section classes="work-history">
+        <SectionHeading>Work History:</SectionHeading>
+
+        <WorkHistory
+          name="AtreNet"
+          website="atre.net"
+          url="https://atre.net"
+          start="2016"
+          end="present"
+          title="VP of Engineering"
+        >
+          <p>At AtreNet I’ve been working as the leader of the engineering department as well as being actively involved with development work. As is the nature for smaller companies I’ve had to embrace performing across a spectrum of responsibilities and aspects of building and maintaining corporate websites.</p>
+          <WorkHistoryLineGroup>
+            <WorkHistoryLineItem>Creating, upgrading and maintaining WordPress-based platform that was the bread-and-butter for our clients. Enabled fast development while allowing clients to maintain without too much hands-on coding. Constant iterations and improvements over the years for functionality, performance and security. Allowed for high end designs to come to life. Balance of best practices, ease-of-use, client capabilities and developer efficiency.</WorkHistoryLineItem>
+          </WorkHistoryLineGroup>
+
+        </WorkHistory>
+      </Section>
+
     </div>
   );
 }
 
 export default App;
+
+
+function ProfileTitle({ title, icon }) {
+  return (
+    <div className="title uppercase px-5 border-r-2 flex last-of-type:border-r-0 border-stone-400 font-bold text-sm tracking-wide">
+      <h3>{title}</h3>
+    </div>
+  );
+}
+
+function ProfileInfo({ content, icon, url }) {
+  return (
+    <div className="info text-white px-5 border-r-2 flex content-center last-of-type:border-r-0 border-slate-500">
+      <div className="info__media max-w-8">
+        <img src={icon} alt={content} className="size-full pr-2" />
+      </div>
+      <h3 className="text-nowrap flex items-center font-bold tracking-wide">
+        {url ? (
+          <a href={url} target="_blank" rel="noreferrer">
+            {content}
+          </a>
+        ) : (
+          content
+        )}
+      </h3>
+    </div>
+  );
+}
+
+function Skills() {
+  const primarySkills = skills.filter(skill =>
+    skill.type === 'primary'
+  );
+  const listItems = primarySkills.map(skill =>
+    <li key={skill.id} className="mr-5">{skill.name}</li>
+  );
+  return <ul className="text-xl font-semibold flex flex-wrap">{listItems}</ul>
+}
+
+function SkillsSecondary() {
+  const secondarySkills = skills.filter(skill =>
+    skill.type === 'secondary'
+  );
+  const listItems = secondarySkills.map(skill =>
+    <li key={skill.id} className="mr-5">{skill.name}</li>
+  );
+  return <ul className="flex flex-wrap mt-5">{listItems}</ul>
+}
+
+function Section({ children, classes, container = true }) {
+  return (
+    <section className={classes + " mt-10"}>
+      <div className="container max-w-screen-xl mx-auto px-5">{children}</div>
+    </section>
+  )
+}
+
+function SectionHeading({ children, classes }) {
+  return (
+    <h2 className={classes + " text-2xl font-bold py-2 mb-2 border-b-2 border-gray-400 font-serif text-gray-700"}>
+      {children}
+    </h2>
+  )
+}
+
+function WorkHistory({ name, website, url, start, end, title, children }) {
+  return (
+    <div className="work-history__item mb-2">
+      <div className="work-history__heading flex align-center content-end">
+        <h3 className="text-2xl">{name}</h3>
+        {website &&
+            <h5 className="flex align-bottom content-end flex-wrap ml-2 mb-0.5">
+              (<a href={url} target="_blank" rel="noreferrer" className="text-sky-600">{website}</a>)
+            </h5>
+        }
+      </div>
+      <h4 className="work-history__title text-lg uppercase font-bold">{title}</h4>
+      <div className="work-history__timeline mb-3">
+        <strong>{start} - {end}</strong>
+      </div>
+        {children}
+    </div>
+  )
+}
+
+function WorkHistoryLineGroup({ children }) {
+  return (
+    <ul className="work-history__line-items marker:text-sky-400 list-outside list-disc ml-6">
+      {children}
+    </ul>
+    )
+}
+function WorkHistoryLineItem({ children }) {
+  return (
+    <li className="text-sm mt-1 mb-1">{children}</li>
+  )
+}
---
+++ src/skills.js
@@ -0,0 +1,164 @@
+export const skills = [
+	{
+		"id": 1,
+		"name": "JavaScript",
+		"type": "primary"
+	},
+	{
+		"id": 2,
+		"name": "React",
+		"type": "primary"
+	},
+	{
+		"id": 3,
+		"name": "HTML5",
+		"type": "primary"
+	},
+	{
+		"id": 4,
+		"name": "SCSS",
+		"type": "primary"
+	},
+	{
+		"id": 5,
+		"name": "Tailwind",
+		"type": "primary"
+	},
+	{
+		"id": 6,
+		"name": "CSS & SVG Animations",
+		"type": "primary"
+	},
+	{
+		"id": 7,
+		"name": "UI/UX",
+		"type": "primary"
+	},
+	{
+		"id": 8,
+		"name": "Photoshop/XD/Figma",
+		"type": "primary"
+	},
+	{
+		"id": 9,
+		"name": "Responsive Design",
+		"type": "primary"
+	},
+	{
+		"id": 10,
+		"name": "Performance Optimization",
+		"type": "primary"
+	},
+	{
+		"id": 11,
+		"name": "Website Workflows",
+		"type": "primary"
+	},
+	{
+		"id": 12,
+		"name": "CI/CD",
+		"type": "primary"
+	},
+	{
+		"id": 13,
+		"name": "WordPress",
+		"type": "primary"
+	},
+	{
+		"id": 14,
+		"name": "Google Tag Manager",
+		"type": "primary"
+	},
+	{
+		"id": 15,
+		"name": "Git",
+		"type": "primary"
+	},
+	{
+		"id": 16,
+		"name": "jQuery",
+		"type": "primary"
+	},
+	{
+		"id": 17,
+		"name": "Reading Documentation",
+		"type": "primary"
+	},
+	{
+		"id": 18,
+		"name": "AI (ChatGPT, Bard, MidJourney, StableDiffusion)",
+		"type": "secondary"
+	},
+	{
+		"id": 19,
+		"name": "PHP",
+		"type": "secondary"
+	},
+	{
+		"id": 20,
+		"name": "Illustrator",
+		"type": "secondary"
+	},
+	{
+		"id": 21,
+		"name": "After Effects",
+		"type": "secondary"
+	},
+	{
+		"id": 22,
+		"name": "Git and GitHub Actions",
+		"type": "secondary"
+	},
+	{
+		"id": 23,
+		"name": "Website concepting & strategy",
+		"type": "secondary"
+	},
+	{
+		"id": 24,
+		"name": "Marketing campaigns",
+		"type": "secondary"
+	},
+	{
+		"id": 25,
+		"name": "Google Analytics (UA, GA4)",
+		"type": "secondary"
+	},
+	{
+		"id": 26,
+		"name": "Website security",
+		"type": "secondary"
+	},
+	{
+		"id": 27,
+		"name": "AI Prompts",
+		"type": "secondary"
+	},
+	{
+		"id": 28,
+		"name": "Lottie.js",
+		"type": "secondary"
+	},
+	{
+		"id": 29,
+		"name": "Localization/WPML",
+		"type": "secondary"
+	},
+	{
+		"id": 30,
+		"name": "GDPR Compliance",
+		"type": "secondary"
+	},
+	{
+		"id": 31,
+		"name": "QA Workflows",
+		"type": "secondary"
+	},
+	{
+		"id": 32,
+		"name": "MySQL/MongoDB",
+		"type": "secondary"
+	}
+]
+
+
